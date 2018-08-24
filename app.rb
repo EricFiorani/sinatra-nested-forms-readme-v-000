b4781a7291979d2dfab1f3efe27d1c637b857f69
Eric Fiorani <efiorani305@gmail.com>
2018-08-24 14:37:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,12 +6,14 @@ class Application::SinatraBase
 
   post '/student' do
     @student = Student.new(params[:student])
-   
+
     params[:student][:courses].each do |details|
       Course.new(details)
     end
-   
+
     @courses = Course.all
-   
+
     erb :student
   end
+
+end
